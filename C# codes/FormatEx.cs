47f2aa204ef5425557e7809e9d3c55721918c317
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:13:43 +0530

    FormatEx.cs

--- C# codes/FormatEx.cs
+++ C# codes/FormatEx.cs
@@ -1,28 +1,28 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    public class FormatEx
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Enter a Number: ");
-//            string number = Console.ReadLine();  // When the entered value in number is an alphabetical string that can not be converted into int so we will get Format Exception
+namespace C__codes
+{
+   public class FormatEx
+   {
+       static void Main(string[] args)
+       {
+           Console.WriteLine("Enter a Number: ");
+           string number = Console.ReadLine();  // When the entered value in number is an alphabetical string that can not be converted into int so we will get Format Exception
 
-//            try
-//            {
-//                int num = int.Parse(number); // Converts string into int
-//                Console.WriteLine("Number is: " + num);
-//            }
-//            catch (FormatException ex)
-//            {
-//                Console.WriteLine("Can not convert higher data type to lower data type");
-//                Console.WriteLine(ex.Message);
-//            }
-//        }
-//    }
-//}
+           try
+           {
+               int num = int.Parse(number); // Converts string into int
+               Console.WriteLine("Number is: " + num);
+           }
+           catch (FormatException ex)
+           {
+               Console.WriteLine("Can not convert higher data type to lower data type");
+               Console.WriteLine(ex.Message);
+           }
+       }
+   }
+}
