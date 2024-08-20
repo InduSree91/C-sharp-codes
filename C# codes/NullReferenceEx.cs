010246852914acbbbd2f68d00b6ee595512d6171
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:28:43 +0530

    NullReferenceEx.cs

--- C# codes/NullReferenceEx.cs
+++ C# codes/NullReferenceEx.cs
@@ -1,34 +1,34 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    public class NullReferenceEx
-//    {
-//        static void Main(string[] args)
-//        {
+namespace C__codes
+{
+   public class NullReferenceEx
+   {
+       static void Main(string[] args)
+       {
 
 
-//            try
-//            {
-//                string name = "Indu";
-//                //string name = null; // Name should not be given null as the value
+           try
+           {
+               string name = "Indu";
+               //string name = null; // Name should not be given null as the value
 
-//                Console.WriteLine(name.Length);
-//            }
-//            catch (NullReferenceException ex)
-//            {
-//                Console.WriteLine("Input string is Null");
-//                Console.WriteLine(ex.Message);
-//            }
+               Console.WriteLine(name.Length);
+           }
+           catch (NullReferenceException ex)
+           {
+               Console.WriteLine("Input string is Null");
+               Console.WriteLine(ex.Message);
+           }
 
-//            Console.WriteLine("----------------------");
+           Console.WriteLine("----------------------");
 
-//            Console.WriteLine("This line will run only when the error is handled.");
+           Console.WriteLine("This line will run only when the error is handled.");
 
-//        }
-//    }
-//}
+       }
+   }
+}
