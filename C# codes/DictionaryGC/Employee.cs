774991da470bd6e19af88867607e82ad71f2af62
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:10:22 +0530

    Employee.cs

--- C# codes/DictionaryGC/Employee.cs
+++ C# codes/DictionaryGC/Employee.cs
@@ -1,17 +1,17 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes.DictionaryGC
-//{
-//    public class Employee
-//    {
-//        public string name { get; set; }
-//        public string designation { get; set; }
-//        public int age { get; set; }
-//        public int id { get; set; }
+namespace C__codes.DictionaryGC
+{
+   public class Employee
+   {
+       public string name { get; set; }
+       public string designation { get; set; }
+       public int age { get; set; }
+       public int id { get; set; }
 
-//    }
-//}
+   }
+}
