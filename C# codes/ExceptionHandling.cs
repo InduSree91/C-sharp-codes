926e5fb33dd6f240a17f35c1b1c34707aa5383a8
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:13:28 +0530

    ExceptionHandling.cs

--- C# codes/ExceptionHandling.cs
+++ C# codes/ExceptionHandling.cs
@@ -1,33 +1,33 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    public class ExceptionHandling
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Enter first number: ");
-//            int num1 = int.Parse(Console.ReadLine());
-//            Console.WriteLine("Enter second number: ");
-//            int num2 = int.Parse(Console.ReadLine());
+namespace C__codes
+{
+   public class ExceptionHandling
+   {
+       static void Main(string[] args)
+       {
+           Console.WriteLine("Enter first number: ");
+           int num1 = int.Parse(Console.ReadLine());
+           Console.WriteLine("Enter second number: ");
+           int num2 = int.Parse(Console.ReadLine());
 
-//            try
-//            {
-//                int result = num1 / num2;
+           try
+           {
+               int result = num1 / num2;
 
-//                Console.WriteLine("Result is: " + result);
-//            }
-//            catch (DivideByZeroException ex)
-//            {
-//                Console.WriteLine("You can not divide a number by zero");
-//                Console.WriteLine(ex.Message); // Prints the message that C# prints
-//            }
+               Console.WriteLine("Result is: " + result);
+           }
+           catch (DivideByZeroException ex)
+           {
+               Console.WriteLine("You can not divide a number by zero");
+               Console.WriteLine(ex.Message); // Prints the message that C# prints
+           }
 
-//            Console.WriteLine("Ther is no error so this line is printing");
-//        }
-//    }
-//}
+           Console.WriteLine("Ther is no error so this line is printing");
+       }
+   }
+}
