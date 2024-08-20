82c8c5771714eb55bbdac979bbd3a5c66a4d3667
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:12:47 +0530

    Encapsulation.cs

--- C# codes/Encapsulation.cs
+++ C# codes/Encapsulation.cs
@@ -1,41 +1,41 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    class NewClass
-//    {
-//        private string StudentName;
-//        private int StudentAge;
+namespace C__codes
+{
+   class NewClass
+   {
+       private string StudentName;
+       private int StudentAge;
 
-//        public string Name
-//        {
-//            get { return StudentName; }
-//            set { StudentName = value; }
-//        }
+       public string Name
+       {
+           get { return StudentName; }
+           set { StudentName = value; }
+       }
 
-//        public int Age
-//        {
-//            get { return StudentAge; }
-//            set { StudentAge = value; }
-//        }
-//    }
+       public int Age
+       {
+           get { return StudentAge; }
+           set { StudentAge = value; }
+       }
+   }
 
 
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            NewClass obj1 = new NewClass();
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           NewClass obj1 = new NewClass();
 
-//            obj1.Name = "Sai";
-//            obj1.Age = 27;
+           obj1.Name = "Sai";
+           obj1.Age = 27;
 
-//            Console.WriteLine("Name : " + obj1.Name);
-//            Console.WriteLine("Age : " + obj1.Age);
-//        }
-//    }
-//}
+           Console.WriteLine("Name : " + obj1.Name);
+           Console.WriteLine("Age : " + obj1.Age);
+       }
+   }
+}
