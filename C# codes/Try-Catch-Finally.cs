5db9a2bae85d0d8625415c1ee5984090df11782b
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:29:30 +0530

    Try-Catch-Finally.cs

--- C# codes/Try-Catch-Finally.cs
+++ C# codes/Try-Catch-Finally.cs
@@ -1,33 +1,33 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    public class Try_Catch_Finally
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Start the code");
+namespace C__codes
+{
+   public class Try_Catch_Finally
+   {
+       static void Main(string[] args)
+       {
+           Console.WriteLine("Start the code");
 
-//            int i = 0;
+           int i = 0;
 
-//            try
-//            {
-//                int result = 10 / i;
-//            }
-//            catch (DivideByZeroException ex)
-//            {
-//                Console.WriteLine("Number can not be divided by Zero");
+           try
+           {
+               int result = 10 / i;
+           }
+           catch (DivideByZeroException ex)
+           {
+               Console.WriteLine("Number can not be divided by Zero");
 
-//                throw;
-//            }
-//            finally
-//            {
-//                Console.WriteLine("Completed running");
-//            }
-//        }
-//    }
-//}
+               throw;
+           }
+           finally
+           {
+               Console.WriteLine("Completed running");
+           }
+       }
+   }
+}
