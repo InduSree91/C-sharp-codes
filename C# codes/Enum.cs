d081f5e685178cda6af58754fa04b22ce054aa27
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:13:09 +0530

    Enum.cs

--- C# codes/Enum.cs
+++ C# codes/Enum.cs
@@ -1,84 +1,84 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    public enum TraficLight
-//    {
-//        Red,
-//        Green,
-//        Yellow
-//    }
+namespace C__codes
+{
+   public enum TraficLight
+   {
+       Red,
+       Green,
+       Yellow
+   }
 
-//    public class Enum
-//    {
-//        public enum DaysInWeek
-//        {
-//            Sunday,
-//            Monday,
-//            Tuesday,
-//            Wednesday,
-//            Thursday,
-//            Friday,
-//            Saturday
-//        }
+   public class Enum
+   {
+       public enum DaysInWeek
+       {
+           Sunday,
+           Monday,
+           Tuesday,
+           Wednesday,
+           Thursday,
+           Friday,
+           Saturday
+       }
 
-//        [Flags]  // Represents combination of values using bitwise operations
-//        public enum FileName
-//        {
-//            Name = 0000,
-//            Age = 0100,
-//            Gender = 0010,
-//            Birthday = 0001,
-//            Contains = Name | Age | Gender | Birthday
-//        }
+       [Flags]  // Represents combination of values using bitwise operations
+       public enum FileName
+       {
+           Name = 0000,
+           Age = 0100,
+           Gender = 0010,
+           Birthday = 0001,
+           Contains = Name | Age | Gender | Birthday
+       }
 
 
 
-//        public static void Main()
-//        {
-//            //Console.WriteLine((DaysInWeek)4); // To print the enum with the number auto assignment
+       public static void Main()
+       {
+           //Console.WriteLine((DaysInWeek)4); // To print the enum with the number auto assignment
 
-//            //DaysInWeek.Tuesday = (DaysInWeek)2;  // both are the same things we can also indicate the days with the index values
+           //DaysInWeek.Tuesday = (DaysInWeek)2;  // both are the same things we can also indicate the days with the index values
 
-//            //DaysInWeek Today = DaysInWeek.Sunday;
+           //DaysInWeek Today = DaysInWeek.Sunday;
 
-//            //DaysInWeek Today = (DaysInWeek)7;
+           //DaysInWeek Today = (DaysInWeek)7;
 
-//            //Console.WriteLine(Today);
+           //Console.WriteLine(Today);
 
-//            //Console.WriteLine((FileName)0001);
+           //Console.WriteLine((FileName)0001);
 
-//            //Console.WriteLine((FileName)0111);
+           //Console.WriteLine((FileName)0111);
 
-//            //FileName file = FileName.Age | FileName.Gender;
+           //FileName file = FileName.Age | FileName.Gender;
 
-//            //Console.WriteLine(file);
+           //Console.WriteLine(file);
 
-//            //if ((FileName.Age & FileName.Gender) == FileName.Name)
-//            //{
-//            //    Console.WriteLine("Name");
-//            //}
+           //if ((FileName.Age & FileName.Gender) == FileName.Name)
+           //{
+           //    Console.WriteLine("Name");
+           //}
 
-//            TraficLight CurrentLight = TraficLight.Green;
+           TraficLight CurrentLight = TraficLight.Green;
 
-//            switch (CurrentLight)
-//            {
-//                case TraficLight.Red:
-//                    Console.WriteLine("Stop");
-//                    break;
+           switch (CurrentLight)
+           {
+               case TraficLight.Red:
+                   Console.WriteLine("Stop");
+                   break;
 
-//                case TraficLight.Green:
-//                    Console.WriteLine("Go");
-//                    break;
-//                case TraficLight.Yellow:
-//                    Console.WriteLine("Wait");
-//                    break;
-//            }
+               case TraficLight.Green:
+                   Console.WriteLine("Go");
+                   break;
+               case TraficLight.Yellow:
+                   Console.WriteLine("Wait");
+                   break;
+           }
 
-//        }
-//    }
-//}
+       }
+   }
+}
