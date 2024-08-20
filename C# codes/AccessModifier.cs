534e19b7fc6755f23e6ef39e1636907e60abb6f3
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:10:01 +0530

    AccessModifier.cs

--- C# codes/AccessModifier.cs
+++ C# codes/AccessModifier.cs
@@ -1,26 +1,25 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    public class AccessModifier
-//    {
-//        class Car
-//        {
-//            private string model = "Skoda";  // only be accessed within the same class
+namespace C__codes
+{
+   public class AccessModifier
+   {
+       class Car
+       {
+           private string model = "Skoda";  // only be accessed within the same class
 
-//            // public access modifier, is accessible for all the classes
+           // public access modifier, is accessible for all the classes
 
-//            static void Main(string[] args)
-//            {
-//                Car car = new Car();
+           static void Main(string[] args)
+           {
+               Car car = new Car();
 
-//                Console.WriteLine(car.model);
-//            }
-//        }
-//    }
-//}
-//// all access modifiers
+               Console.WriteLine(car.model);
+           }
+       }
+   }
+}
