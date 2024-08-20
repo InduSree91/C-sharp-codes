c9e516170b663a2e47b2718d2f145b0a794b850b
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:11:00 +0530

    AsyncAwait.cs

--- C# codes/AsyncAwait.cs
+++ C# codes/AsyncAwait.cs
@@ -1,29 +1,29 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    public class AsyncAwait
-//    {
-//        static async Task Main(string[] args)
-//        {
-//            Console.WriteLine("Starting.....");
+namespace C__codes
+{
+   public class AsyncAwait
+   {
+       static async Task Main(string[] args)
+       {
+           Console.WriteLine("Starting.....");
 
-//            await DoWorkAsync();
+           await DoWorkAsync();
 
-//            Console.WriteLine("Completed");
-//        }
+           Console.WriteLine("Completed");
+       }
 
-//        static async Task DoWorkAsync()
-//        {
-//            Console.WriteLine("Wait, Task in Progress");
+       static async Task DoWorkAsync()
+       {
+           Console.WriteLine("Wait, Task in Progress");
 
-//            await Task.Delay(1000);
+           await Task.Delay(1000);
 
-//            Console.WriteLine("Task Completed");
-//        }
-//    }
-//}
+           Console.WriteLine("Task Completed");
+       }
+   }
+}
