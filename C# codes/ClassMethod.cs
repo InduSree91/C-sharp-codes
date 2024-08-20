5eea06a806705b9b724e81c18f2d685bed1decbf
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:11:26 +0530

    ClassMethod.cs

--- C# codes/ClassMethod.cs
+++ C# codes/ClassMethod.cs
@@ -1,36 +1,36 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    public class ClassMethod
-//    {
-//        class Vehicle
-//        {
-//            public string color;
-//            public int NoOfWheels;
+namespace C__codes
+{
+   public class ClassMethod
+   {
+       class Vehicle
+       {
+           public string color;
+           public int NoOfWheels;
 
-//        }
+       }
 
-//        class Method
-//        {
-//            static void Main(string[] args)
-//            {
-//                Vehicle v = new Vehicle();
-//                v.color = "red";
-//                v.NoOfWheels = 4;
+       class Method
+       {
+           static void Main(string[] args)
+           {
+               Vehicle v = new Vehicle();
+               v.color = "red";
+               v.NoOfWheels = 4;
 
-//                Vehicle v2 = new Vehicle();
-//                v2.color = "yellow";
-//                v2.NoOfWheels = 2;
+               Vehicle v2 = new Vehicle();
+               v2.color = "yellow";
+               v2.NoOfWheels = 2;
 
-//                Console.WriteLine(v.color);
-//                Console.WriteLine(v2.NoOfWheels);
-//            }
-//        }
-//    }
-//}
+               Console.WriteLine(v.color);
+               Console.WriteLine(v2.NoOfWheels);
+           }
+       }
+   }
+}
 
