1862b50ca10f65ccf78b8813d31556e1e5268d01
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:08:49 +0530

    Abstraction.cs

--- C# codes/Abstraction.cs
+++ C# codes/Abstraction.cs
@@ -1,43 +1,43 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    public abstract class Shape
-//    {
-//        public abstract double GetArea();
-//    }
+namespace C__codes
+{
+   public abstract class Shape
+   {
+       public abstract double GetArea();
+   }
 
-//    public class Circle : Shape
-//    {
-//        public double Radius { get; set; }
-//        public override double GetArea()
-//        {
-//            return Math.PI * Radius * Radius;
-//        }
-//    }
+   public class Circle : Shape
+   {
+       public double Radius { get; set; }
+       public override double GetArea()
+       {
+           return Math.PI * Radius * Radius;
+       }
+   }
 
-//    public class Square : Shape
-//    {
-//        public double side { get; set; }
-//        public override double GetArea()
-//        {
-//            return side * side;
-//        }
-//    }
+   public class Square : Shape
+   {
+       public double side { get; set; }
+       public override double GetArea()
+       {
+           return side * side;
+       }
+   }
 
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Shape myShape = new Circle { Radius = 3.74 };
-//            Console.WriteLine(myShape.GetArea());
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           Shape myShape = new Circle { Radius = 3.74 };
+           Console.WriteLine(myShape.GetArea());
 
-//            Shape myShape2 = new Square { side = 4 };
-//            Console.WriteLine(myShape2.GetArea());
-//        }
-//    }
-//}
+           Shape myShape2 = new Square { side = 4 };
+           Console.WriteLine(myShape2.GetArea());
+       }
+   }
+}
