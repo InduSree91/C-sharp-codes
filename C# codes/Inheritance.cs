e59bc709a710a0a82aa1ee148be9fae5e6b427bd
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:28:09 +0530

    Inheritance.cs

--- C# codes/Inheritance.cs
+++ C# codes/Inheritance.cs
@@ -1,41 +1,41 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Car myCar = new Car { Wheels = 4, Color = "Black", Brand = "Toyota", Model = "Corolla", Year = 2024 };
-//            myCar.Start();
-//            myCar.Driving();
-//        }
-//    }
+namespace C__codes
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+           Car myCar = new Car { Wheels = 4, Color = "Black", Brand = "Toyota", Model = "Corolla", Year = 2024 };
+           myCar.Start();
+           myCar.Driving();
+       }
+   }
 
-//    class Vehicle
-//    {
-//        public int Wheels { get; set; }
-//        public string Color { get; set; }
+   class Vehicle
+   {
+       public int Wheels { get; set; }
+       public string Color { get; set; }
 
-//        public void Driving()
-//        {
-//            Console.WriteLine("Driver is driving the Vehicle");
-//        }
-//    }
+       public void Driving()
+       {
+           Console.WriteLine("Driver is driving the Vehicle");
+       }
+   }
 
-//    class Car : Vehicle
-//    {
-//        public string Brand { get; set; }
-//        public string Model { get; set; }
-//        public int Year { get; set; }
+   class Car : Vehicle
+   {
+       public string Brand { get; set; }
+       public string Model { get; set; }
+       public int Year { get; set; }
 
-//        public void Start()
-//        {
-//            Console.WriteLine("Car has started");
-//        }
-//    }
-//}
+       public void Start()
+       {
+           Console.WriteLine("Car has started");
+       }
+   }
+}
