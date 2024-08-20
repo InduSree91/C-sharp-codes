065ebe31d4c53beececf02d75d6de376290c34de
InduSree91 <indusree.gandikota@solvencia.in>
2024-08-20 16:11:42 +0530

    ClassObject.cs

--- C# codes/ClassObject.cs
+++ C# codes/ClassObject.cs
@@ -1,32 +1,32 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace C__codes
-//{
-//    public class Program
-//    {
+namespace C__codes
+{
+   public class Program
+   {
 
-//        static void Main(string[] args)
-//        {
-//            Car myCar = new Car { Brand = "Toyota", Model = "Corolla", Year = 2024 };
-//            myCar.Start();
-//        }
-//    }
+       static void Main(string[] args)
+       {
+           Car myCar = new Car { Brand = "Toyota", Model = "Corolla", Year = 2024 };
+           myCar.Start();
+       }
+   }
 
-//    public class Car
-//    {
-//        public string Brand { get; set; }
-//        public string Model { get; set; }
-//        public int Year { get; set; }
+   public class Car
+   {
+       public string Brand { get; set; }
+       public string Model { get; set; }
+       public int Year { get; set; }
 
-//        public void Start()
-//        {
-//            Console.WriteLine($"The car of the brand {Brand} and model {Model} manufactured in the year {Year} ");
+       public void Start()
+       {
+           Console.WriteLine($"The car of the brand {Brand} and model {Model} manufactured in the year {Year} ");
 
-//            Console.WriteLine("Car has Started");
-//        }
-//    }
-//}
+           Console.WriteLine("Car has Started");
+       }
+   }
+}
